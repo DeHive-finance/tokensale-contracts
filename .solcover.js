267fed2c57f69134e578e6cdc3065cde0a741b74
Artem <artem.m@blaize.tech>
2021-03-15 15:37:02 +0300

    Added solcover file. Aborted gitignore

---
+++ .solcover.js
@@ -0,0 +1,3 @@
+module.exports = {
+    skipFiles: ['Test/DeHiveTokensaleMock.sol', 'Test/DeHiveTokensaleTest.sol', 'Test/TestERC20.sol', 'Test/TestToken.sol']
+  };
---
+++ contracts/Test/DeHiveTokensaleMock.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.6.12;
+
+import "../DeHiveTokensale.sol";
+
+contract DeHiveTokensaleMock is DeHiveTokensale {
+
+    function initializeMock(address _DAIToken,
+        address _USDTToken,
+        address _NUXToken,
+        address treasury,
+        uint _vestingStart,
+        uint _vestingDuration,
+        uint _purchasedWithNUX,
+        uint _purchasedPreSale,
+        uint _purchasedPublicSale,
+        address dhv) public initializer {
+        this.initialize(_DAIToken,
+            _USDTToken,
+            _NUXToken,
+            treasury,
+            _vestingStart,
+            _vestingDuration,
+            _purchasedWithNUX,
+            _purchasedPreSale,
+            _purchasedPublicSale,
+            dhv);
+    }
+    function adminSetRatesMock(address _token, uint256 _rate) public {
+        this.adminSetRates(_token, _rate);
+    }
+    function adminSetTreasuryMock(address _treasury) public {
+        this.adminSetTreasury(_treasury);
+    }
+     function adminPauseMock() public {
+        this.adminPause();
+    }
+    function adminUnpauseMock() public {
+        this.adminUnpause();
+    }
+    function getUSDTToken() public view returns(address){
+        return USDTToken;
+    }
+    function getDAIToken() public view returns(address){
+        return DAIToken;
+    }
+    function getNUXToken() public view returns(address){
+        return NUXToken;
+    }
+}
