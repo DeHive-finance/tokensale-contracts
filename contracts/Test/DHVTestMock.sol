07ac614d8f1a5c7e3a2610f0bc11a101f9ee2ee3
Artem <artem.m@blaize.tech>
2021-03-16 19:03:33 +0300

    Mint function and coverage

---
+++ contracts/Test/DHVTestMock.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.12;
+
+import "../DHVToken.sol";
+
+contract DHVTestMock is DHVToken{
+    
+    function setHardcap(uint256 _hardcap) public{
+        hardCap=_hardcap;
+    }
+    function mintMock(address _receiver, uint _amount) public{
+        this.mint(_receiver, _amount);
+    }
+
+}
